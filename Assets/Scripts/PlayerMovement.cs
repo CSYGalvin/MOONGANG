ebff270ebe49d69bb6f6023838d9216749dfb4a7
Galvin Chan <csygalvin@gmail.com>
2021-05-18 16:20:09 +0800

    added basic PlayerMovement script

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float Speed;
+    public Rigidbody2D body;
+
+    public Animator anim;
+
+    public float jumpForce = 10f;
+    public Transform feet;
+    public LayerMask groundLayers;
+
+    float mx;
+    // Start is called before the first frame update
+    void Start()
+    {
+        jumpForce = 15f;
+        Speed = 5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mx = Input.GetAxisRaw("Horizontal");
+        if (Input.GetButtonDown("Jump") && isGrounded())
+        {
+            Jump();
+        }
+        if (mx >= 0f)
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+        else
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+
+        anim.SetBool("isRunning", Mathf.Abs(mx) > 0.05f);
+        anim.SetBool("isGrounded", isGrounded());
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 movement = new Vector2(mx * Speed, body.velocity.y);
+
+        body.velocity = movement;
+    }
+
+    void Jump()
+    {
+        Vector2 movement = new Vector2(body.velocity.x, jumpForce);
+
+        body.velocity = movement;
+    }
+
+    public bool isGrounded()
+    {
+        Collider2D groundCheck = Physics2D.OverlapCircle(feet.position, 1f, groundLayers);
+        return groundCheck != null;
+    }
+}

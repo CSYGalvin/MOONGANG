b4179fc634a1c3a3006761646723196e93acea3c
Galvin <e0543848@u.nus.edu>
2021-07-26 18:52:44 +0800

    add throw animation, throwing speed is affected by players speed

--- Assets/Scripts/PlayerTeleport.cs
+++ Assets/Scripts/PlayerTeleport.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class PlayerTeleport : MonoBehaviour
 {
     public GameObject teleportBallPrefab;
+    public Animator anim;
+    public float y_speed;
+    public float x_speed;
+    
     private GameObject teleportBall;
     
     // Start is called before the first frame update
@@ -18,9 +22,12 @@ public class PlayerTeleport : MonoBehaviour
     {
         if(Input.GetButtonDown("Teleport")){
             if(teleportBall == null){
-                Vector3 pos = new Vector3(gameObject.transform.position.x + gameObject.transform.localScale.x/5, gameObject.transform.position.y + 1, gameObject.transform.position.z);
+                int direction = gameObject.transform.localScale.x > 0 ? 1 : -1;
+                Vector3 pos = new Vector3(gameObject.transform.position.x + direction, gameObject.transform.position.y + 1, gameObject.transform.position.z + 1);
                 teleportBall = Instantiate(teleportBallPrefab, pos , Quaternion.identity);
-                teleportBall.GetComponent<Rigidbody2D>().velocity = new Vector2(gameObject.transform.localScale.x * 3, 10);
+                anim.SetTrigger("Throwing");
+                teleportBall.GetComponent<Rigidbody2D>().velocity = gameObject.GetComponent<Rigidbody2D>().velocity + new Vector2(direction * x_speed, y_speed);
+
             }else{
                 gameObject.transform.position = teleportBall.transform.position; 
                 Destroy(teleportBall);
--- Assets/Scripts/TeleportBall.cs
+++ Assets/Scripts/TeleportBall.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class TeleportBall : MonoBehaviour
 {
+    public LayerMask ground;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,11 +14,13 @@ public class TeleportBall : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     // void OnCollisionEnter2D(Collision2D collision){
     //     if(collision.gameObject.tag != "Player"){
     //         Destroy(gameObject);
     //     }
     // }
+
+
 }
